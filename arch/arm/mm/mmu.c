9c74ff7ac6627f1407b49c65aa4c5ba5ce2f8f68
Russell King <rmk+kernel@arm.linux.org.uk>
2013-07-31 21:58:56 +0100

    BACKPORT: ARM: make vectors page inaccessible from userspace
    (cherry pick from commit a5463cd3435475386cbbe7b06e01292ac169d36f)
    
    If kuser helpers are not provided by the kernel, disable user access to
    the vectors page.  With the kuser helpers gone, there is no reason for
    this page to be visible to userspace.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Bug: 9674955
    Bug: 25773781
    Change-Id: I04b5bd8acda48f6bf84d42863aed1aaa81dc132f

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -1233,9 +1233,12 @@ static void __init devicemaps_init(struct machine_desc *mdesc)
 	map.pfn = __phys_to_pfn(virt_to_phys(vectors));
 	map.virtual = 0xffff0000;
 	map.length = PAGE_SIZE;
+#ifdef CONFIG_KUSER_HELPERS
 	map.type = MT_HIGH_VECTORS;
-	create_mapping(&map);
-
+#else
+	map.type = MT_LOW_VECTORS;
+#endif
+	create_mapping(&map, false);
 	if (!vectors_high()) {
 		map.virtual = 0;
 		map.length = PAGE_SIZE * 2;
